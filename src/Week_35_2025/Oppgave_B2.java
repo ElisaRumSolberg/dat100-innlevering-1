81aa47eee4dd7538466651925ea84435446c4fc2
ElisaRumSolberg <rumeysaince07@gmail.com>
2025-09-02 14:00:19 +0100

    week36

---
+++ src/Week_35_2025/Oppgave_B2.java
@@ -0,0 +1,26 @@
+package Week_35_2025;
+
+public class Oppgave_B2 {
+    public static void main(String[] args) {
+        /*
+            int a = 5;
+            String s = "Hei";
+            double b = 4.2; // b = 4.2;  vi må skrive double
+
+            System.out.println(a); // 5
+            System.out.println(s); // Hei
+            System.out.println(b); // 4.2
+            System.out.println(s + a + 3); //Hei53
+            System.out.println(3 + a + s); // 8Hei
+            System.out.println(s + "H");//HeiH //System.out.println(s - "H"); Det er ingen - operasjon på strenger.
+            System.out.println(7 / a);
+            System.out.println(13 - 2 * 5);
+
+            //  int c = 1578;
+           // System.out.println(c.length());
+            System.out.println(s.length());
+            */
+
+            }
+        }
+
